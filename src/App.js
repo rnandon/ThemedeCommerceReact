4a1104461717901be3c619d9ec4bcc2e7f7ec734
Ryan <ryan@ryannd.com>
2021-09-02 14:40:56 -0500

    Implemented first draft of Navbar element, added additional routes to Router switch

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import React from 'react';
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
+import Navbar from './components/Pages/Navbar/Navbar';
+import Home from './components/Pages/Home/Home';
+import Search from './components/Pages/Search/Search';
+import ProductDetail from './components/Pages/ProductDetail/ProductDetail';
+import Login from './components/Pages/Login/Login';
+import Register from './components/Pages/Register/Register';
+
 function App() {
     return (
         <Router>
-            <div className="App">
-                <navbar className="App-header">
-            
-                </navbar>
-            </div>
+            <Navbar />
 
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/search/:searchTerm" component={Search} />
+                <Route path="/ProductDetail/:productId" component={ProductDetail} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
             </Switch>
         </Router>
     );
--- src/components/Elements/Navbar/Navbar.jsx
+++ src/components/Elements/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
 const Navbar = () => {
+    // Store the search values before executing a search
+    let [searchTerm, setSearchTerm] = useState("");
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
     return (
-        <h1>Under construction</h1>
+        <nav class="navbar navbar-dark">
+            <div class="container-fluid py-3">
+                <Link to="/" class="navbar-brand col-sm-3"><h1 id="header">SportsSticks</h1></Link>
+                <form class="d-flex col-sm-5">
+                    <input class="form-control me-2" id="searchbar" type="search" placeholder="Search" value={searchTerm} onChange={handleChange} aria-label="Search" />
+                    <Link to={`/search/${searchTerm}`}>
+                        <button class="btn" id="searchButton" type="submit"><i class="fa fa-search"></i></button>
+                    </Link>
+                </form>
+                <div class="col-sm-2">
+                    { /* Put the login stuff here */ }
+                </div>
+            </div>
+        </nav>
     )
 }
 
