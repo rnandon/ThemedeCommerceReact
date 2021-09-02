a5e33fe77ec43eed8cda2551d5e2f5039b92336d
Ryan <ryan@ryannd.com>
2021-09-02 12:50:50 -0500

    Implemented base structure for React-Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
+import React from 'react';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-        
-            </header>
-        </div>
+        <Router>
+            <div className="App">
+                <navbar className="App-header">
+            
+                </navbar>
+            </div>
+
+            <Switch>
+                <Route path="/" exact component={Home} />
+            </Switch>
+        </Router>
     );
 }
 
 export default App;
+
+const Home = () => {
+    return (
+        <Fragment>
+            <h1>Under construction</h1>
+        </Fragment>
+    )
+}
