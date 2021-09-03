0f1640f22eb7fcfc27ad33652584df464fe1a812
Ryan <ryan@ryannd.com>
2021-09-03 10:05:37 -0500

    Scaffolded AccountTab, Category, Product, Sidebar components

---
+++ src/components/Elements/Category/Category.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Category = () => {
+    return (
+        <h1>Under construction</h1>
+    )
+}
+
+export default Category;
---
+++ src/components/Elements/Sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <h1>Under construction</h1>
+    )
+}
+
+export default Sidebar;
