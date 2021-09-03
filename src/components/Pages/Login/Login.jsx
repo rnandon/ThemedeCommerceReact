d7ace2666656dafefef6cc017432b48bbfb94cc8
Ryan <ryan@ryannd.com>
2021-09-03 12:06:33 -0500

    Added useForm hook, imported basic structure to Login.jsx

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import useForm from '../../../hooks/useForm';
 
 const Login = () => {
+    const sendLogin = () => {
+        return "1";
+    }
+
+    const { formValues, handleChange, handleSubmit } = useForm(sendLogin);
+
     return (
         <h1>Under construction</h1>
     )
