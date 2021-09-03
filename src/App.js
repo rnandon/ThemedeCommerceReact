3212d2cff836b0a86e92fb2a846cf3c9e498165c
Ryan <ryan@ryannd.com>
2021-09-03 11:22:17 -0500

    Added additional routes to app.js switch for Cart, UploadItem, and Account pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
 import Navbar from './components/Elements/Navbar/Navbar';
 import Home from './components/Pages/Home/Home';
@@ -8,7 +9,9 @@ import Search from './components/Pages/Search/Search';
 import ProductDetail from './components/Pages/ProductDetail/ProductDetail';
 import Login from './components/Pages/Login/Login';
 import Register from './components/Pages/Register/Register';
-import jwtDecode from 'jwt-decode';
+import Account from './components/Pages/Account/Account';
+import Cart from './components/Pages/Cart/Cart';
+import UploadItem from './components/Pages/UploadItem/UploadItem';
 
 function App() {
     // Get local login if there is one
@@ -18,13 +21,13 @@ function App() {
         const localUser = jwtDecode(jwt);
         setUser(localUser);
     } catch {
-        
+
     }
 
 
     return (
         <Router>
-            <Navbar />
+            <Navbar user={user} />
 
             <Switch>
                 <Route path="/" exact component={Home} />
@@ -32,6 +35,9 @@ function App() {
                 <Route path="/ProductDetail/:productId" component={ProductDetail} />
                 <Route path="/login" component={Login} />
                 <Route path="/register" component={Register} />
+                <Route path="/account" component={Account} />
+                <Route path="/cart" component={Cart} />
+                <Route path="/Upload" component={UploadItem} />
             </Switch>
         </Router>
     );
