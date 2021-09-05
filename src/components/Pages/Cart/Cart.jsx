8167f69c1d080b9a139bb0f0547010262ba6473a
Ryan <ryan@ryannd.com>
2021-09-05 11:17:48 -0500

    Implemented checkout button and procedures on Cart page

--- src/components/Pages/Cart/Cart.jsx
+++ src/components/Pages/Cart/Cart.jsx
@@ -12,11 +12,46 @@ const Cart = (props) => {
     const token = localStorage.getItem('token');
     const auth = { Authorization: `Bearer ${token}` };
     const [cart, setCart] = useState();
+    const [orderConfirmed, setOrderConfirmed] = useState(false);
 
     async function getCart() {
         let response = await axios.get(cartUri, { headers: auth });
         if (response.data){
             setCart(response.data);
+        }
+    }
+
+    async function checkout() {
+        const currentTime = new Date();
+        const newOrder = {
+            "userid": user.id,
+            "year": currentTime.getFullYear(),
+            "month": currentTime.getUTCMonth(),
+            "day": currentTime.getDate(),
+            "hours": currentTime.getHours(),
+            "minutes": currentTime.getMinutes(),
+            "seconds": currentTime.getSeconds()
+        }
+        let response = await axios.post("https://localhost:44394/api/order", newOrder, { headers: auth });
+        if (response.data){
+            // Response contains order id
+            debugger;
+            const productOrders = cart.map((entry) => {
+                return {
+                    "productid": entry.product.productId,
+                    "quantity": entry.quantity,
+                    "orderid": response.data.orderId
+                };
+            });
+
+            let newOrderResponse = await axios.post("https://localhost:44394/api/productorder/order", productOrders, { headers: auth });
+            if (newOrderResponse){
+                setOrderConfirmed(true);
+                cart.map((entry) => {
+                    axios.delete(`https://localhost:44394/api/cart/${entry.cartId}`, { headers: auth });
+                })
+                getCart();
+            }
         }
     }
 
@@ -40,6 +75,8 @@ const Cart = (props) => {
                     <h1>Your cart:</h1>
                     {cartItems}
                     <h2>Total price: {total}</h2>
+                    <button onClick={checkout} >Checkout</button>
+                    {orderConfirmed && <h2>Order placed!</h2>}
                 </div>
             )
         }
