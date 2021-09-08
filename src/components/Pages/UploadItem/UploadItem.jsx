d69a55cf8753e560d112c1740fdad199a712a437
Ryan <ryan@ryannd.com>
2021-09-07 23:46:23 -0500

    Finished implementing UploadItem page

--- src/components/Pages/UploadItem/UploadItem.jsx
+++ src/components/Pages/UploadItem/UploadItem.jsx
@@ -4,13 +4,40 @@ import axios from 'axios';
 
 import useForm from '../../../hooks/useForm';
 
-const UploadItem = () => {
+const UploadItem = (props) => {
     const history = useHistory();
-    const sendNewProduct = () => {
-        console.log(formValues);
-        // uploadProduct(formValues);
-        history.push("/");
+    const [categories, setCategories] = useState(false);
+    async function getCategories() {
+        // Retrieve all category values stored in the db for filtering
+        let categoryResponse = await axios.get("https://localhost:44394/api/category");
+        if (categoryResponse.data.length >= 0) {
+            const retrievedCategories = categoryResponse.data;
+            const categoryOptions = retrievedCategories.map((category) => {
+                return (
+                        <option value={category.categoryId}>{category.name}</option>
+                        
+                )
+            })
+            setCategories(categoryOptions);
+        }
     }
+    useEffect(() => {getCategories()}, []);
+
+    async function sendNewProduct() {
+        console.log("Sending new product");
+        const token = localStorage.getItem('token');
+        let newProduct = formValues;
+        newProduct.price = parseFloat(newProduct.price);
+        newProduct.userId = props.user.id;
+        console.log(newProduct.categoryId);
+        newProduct.categoryId = parseInt(newProduct.categoryId);
+        let response = axios.post("https://localhost:44394/api/product", formValues, { headers: { Authorization: `Bearer ${token}` }});
+        if (response.data) {
+            console.log("Added new product");
+        }
+        history.push("/account");
+    }
+
     const { formValues, handleChange, handleSubmit } = useForm(sendNewProduct);
 
     return (
@@ -28,10 +55,10 @@ const UploadItem = () => {
                 <label for="descriptionInput" class="form-label">Description</label>
                 <input type="text" name="description" value={formValues.description} onChange={handleChange} class="form-control" id="descriptionInput" />
             </div>
-            <div class="mb-3">
-                <label for="categoryInput" class="form-label">Category</label>
-                <input type="text" name="category" value={formValues.category} onChange={handleChange} class="form-control" id="categoryInput" />
-            </div>
+            <select  name="categoryId" class="form-select" aria-label="Category select" onChange={handleChange} >
+                <option default>Category</option>
+                {categories}
+            </select>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     )
