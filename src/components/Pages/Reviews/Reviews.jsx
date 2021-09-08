b8eec830fcfd017e8d183c61431235e4bdf39c99
Ryan <ryan@ryannd.com>
2021-09-07 22:07:50 -0500

    Implemented Add/See reviews on a product

--- src/components/Pages/Reviews/Reviews.jsx
+++ src/components/Pages/Reviews/Reviews.jsx
@@ -1,35 +1,62 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+import useReviewSection from '../../../hooks/useReviewSection';
 
 
 const Reviews = (props) => {
+    const user = props.user
+
     const productId = props.productId;
+    const [reviews, setReviews] = useState(false);
 
     async function getProductReviews(productId) {
         console.log(productId);
-        try {
-            let response = await axios.get(`https://localhost:44394/api/reviews/${productId}`);
-            return response.data;
-        } catch (ex) {
-            console.log("No Data Found");
+        let response = await axios.get(`https://localhost:44394/api/reviews/${productId}`);
+        if (response.status === 200) {
+            setReviews(response.data);
         }
     }
 
-    const reviews = getProductReviews(productId);
+    const getForm = useReviewSection(productId, user);
 
-    const productReviews = reviews.map((item) => {
-        const review = {
-            rating: item.rating,
-            body: item.body,
-        };
+    useEffect(() => {
+        getProductReviews(productId);
+    }, [])
 
-        return (
-            <div>
-                {productReviews}
-                <p>{review.rating}</p>
-                <p>{review.body}</p>
-            </div>
-        )
-    });
+    if (!(reviews.length >= 0)) {
+        return <h1>Gathering reviews...</h1>
+    } else {
+        let totalReviewPoints = 0;
+        let totalPossiblePoints = 0;
+        if (reviews.length >= 1) {
+            const formattedReviews = reviews.map((review) => {
+                totalPossiblePoints += 5;
+                totalReviewPoints += review.rating;
+                return (
+                    <div>
+                        <h3>{review.rating}/5</h3>
+                        <h4>{review.body}</h4>
+                    </div>
+                )
+            })
+            const overallRating = ( totalReviewPoints / totalPossiblePoints) * 5;
+            return (
+                <div>
+                    {getForm()}
+                    <h2>Overall Rating: {overallRating}/5</h2>
+                    {formattedReviews}
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <h2>No reviews yet.</h2>
+                    {getForm()}
+                </div>
+            )
+        }
+    }
 }
 
 export default Reviews;
